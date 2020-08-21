a6487c4a4802839594b693e22be416254c65d7a3
Roberto Ortega <ingticsjrop@outlook.es>
2020-08-21 08:37:37 -0500

    First commit

---
+++ docs/precache-manifest.9934a3254e8164f99dc17a4412c22223.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d62ce072138e9729b768e3a6c826d3de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "412e3fa0dba9545f133b",
+    "url": "/static/css/main.54a49eac.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "412e3fa0dba9545f133b",
+    "url": "/static/js/main.93db3dd6.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
